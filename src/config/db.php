4f5d039e221784fd39235cf5748b22972cba8aa2
d.egorov <d.egorov@smileexpo.ru>
2020-09-09 09:10:13 +0300

    test Procfile

--- src/config/db.php
+++ src/config/db.php
@@ -1,10 +1,23 @@
 <?php
 
+$server = 'mysql';
+$username = 'root';
+$password = 'toor';
+$db = 'test';
+
+if (getenv("CLEARDB_DATABASE_URL")) {
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"] ?? '', 1);
+}
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mysql;dbname=test',
-    'username' => 'root',
-    'password' => 'toor',
+    'dsn' => 'mysql:host=' . $server . ';dbname=' . $db,
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
     'enableSchemaCache' => true,
     'schemaCacheDuration' => 60,
