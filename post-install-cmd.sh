743d29ec835ec5820738d6ba1c3fd97d05a0dc95
Дмитрий Егоров <desure85@gmail.com>
2020-10-20 23:41:29 +0300

    Update post-install-cmd.sh

--- post-install-cmd.sh
+++ post-install-cmd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  cd src
  COMPOSER_MEMORY_LIMIT=-1 composer install
- php -y yii migrate --interactive
- php -y yii migrate --interactive --migrationPath="@app/modules/webhookTrap/migrations"
+ php yii migrate --interactive=0
+ php yii migrate --interactive=0 --migrationPath="@app/modules/webhookTrap/migrations"
  php yii currency/check
